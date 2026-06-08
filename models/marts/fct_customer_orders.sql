7cff6f954b41af7b79ccd1f9f7e17480aed38e19
anandgupta42 <anand@altimate.ai>
2026-06-08 13:11:07 -0700

    Demo s019 lateral on true join

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -21,7 +21,13 @@ select
     customers.customer_name,
     orders.order_date,
     orders.amount,
+    metrics.customer_order_count,
     orders.status
 from orders
 left join customers
     on orders.customer_id = customers.customer_id
+left join lateral (
+    select count(*) as customer_order_count
+    from {{ ref('stg_orders') }} as order_metrics
+    where order_metrics.customer_id = orders.customer_id
+) as metrics on true
